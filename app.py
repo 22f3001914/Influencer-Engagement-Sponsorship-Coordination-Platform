ec3de05b96236947c67028f4460d880391ce08b3
AvinashShrivastav <avinashshrivastavaofficial@gmail.com>
2024-07-11 06:54:20 +0530

    update

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask
 
-app = Flask(__name__)
+app = None
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+def create_app():
+    app = Flask(__name__)
+    app.debug = True
+    app.app_context().push()
+    return app
 
+app = create_app()
+
+from backend.controllers import *
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
+    
